ba89711202cc92d20f92f62aeed230ddf443bcc8
Haitham Aouati <55059470+haithamaouati@users.noreply.github.com>
2024-04-10 04:07:38 +0100

    Create Eruda.js

---
+++ Eruda.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Eruda
+// @namespace    https://eruda.liriliri.io/
+// @version      0.1
+// @description  Console for mobile browsers
+// @author       Haitham Aouati
+// @run-at       document-start
+// @match        https://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var script = document.createElement('script');
+    script.src = "https://cdn.jsdelivr.net/npm/eruda";
+    document.body.append(script);
+    
+    script.onload = function() {
+        eruda.init();
+    };
+})();
